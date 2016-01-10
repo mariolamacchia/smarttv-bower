0af4ed4be7fa7eb951114e48d0cb9cbd21bdd3c2
Mario Lamacchia <mario@appstrakt.com>
2016-01-10 20:56:11 +0100

    First commit

---
+++ smarttv.js
@@ -0,0 +1,48 @@
+(function(window) {
+  'use strict';
+
+  var smarttv = {};
+  window.smarttv = smarttv;
+
+  // Socket
+  var socket = io.connect('/');
+  smarttv.send = io.send;
+  smarttv.on = io.on;
+
+  // Apps
+  $.get('/api/apps', function(apps) {
+    smarttv.apps = apps;
+  });
+  smarttv.showApp = function(app) {
+    window.location.href = '/' + app;
+  };
+  smarttv.pressKey = function(key) {
+    sendEvent('keyDown', key);
+    sendEvent('keyUp', key);
+
+    function sendEvent(type, key) {
+      $.ajax('http://192.168.0.212:8000/api/inputs', {
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          type: type,
+          keyCode: key
+        })
+      });
+    }
+  };
+
+  // Hidden keyboard
+  $(document).ready(function() {
+    $('body').append(
+      $('<input type=text id="st-hidden-input">')
+        .width(window.innerWidth)
+        .css('position', 'absolute')
+        .css('top', '-100%')
+    );
+  });
+  smarttv.showKeyboard = function() {
+    $('#st-hidden-input').focus();
+  };
+
+})(window);
